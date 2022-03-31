5d2cec85c68e2595dea4bdeff4ce1c6e8bdd8e3a
avo888 <avoevodin8888@gmail.com>
2022-04-01 01:04:16 +0300

    added car class based on vehicle

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,24 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from .base import Vehicle
+from .engine import Engine
+
+
+class Car(Vehicle):
+    """
+    Describes car entity.
+    """
+    def __init__(self):
+        super.__init__()
+        self._engine = Engine()
+
+    @property
+    def engine(self):
+        return self._engine
+
+    def set_engine(self, engine):
+        if isinstance(engine, Engine):
+            self._engine = engine
+        else:
+            raise ValueError

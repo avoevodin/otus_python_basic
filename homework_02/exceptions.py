3d35c6efd3c41de47e016cbfcd70fd3d643439aa
avo888 <avoevodin8888@gmail.com>
2022-03-31 22:05:08 +0300

    added init exceptions classes

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,42 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    """
+    Exception raises when the fuel level is low.
+    """
+    def __init__(self, fuel, message="The fuel level is low."):
+        self.fuel = fuel
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return f'{self.fuel} -> {self.message}'
+
+
+class NotEnoughFuel(Exception):
+    """
+    Exception raises when the fuel level is not enough for further moving.
+    """
+    def __init__(self, fuel, message="The fuel level is not enough."):
+        self.fuel = fuel
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return f'{self.fuel} -> {self.message}'
+
+
+class CargoOverload(Exception):
+    """
+    Exception raises when the vehicle is overloaded with cargo.
+    """
+    def __init__(self, fuel, message="The vehicle is overload."):
+        self.fuel = fuel
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return f'{self.fuel} -> {self.message}'

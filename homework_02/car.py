67fbda2fb159c28cb66f88924919fbb43a82b332
avo888 <avoevodin8888@gmail.com>
2022-04-01 01:39:27 +0300

    fixed typos

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,17 +1,21 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
-from .base import Vehicle
-from .engine import Engine
+from base import Vehicle
+from engine import Engine
 
 
 class Car(Vehicle):
     """
     Describes car entity.
     """
-    def __init__(self):
-        super.__init__()
-        self._engine = Engine()
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._engine = None
+
+    def __repr__(self):
+        parent_repr = super().__repr__()
+        return f'{parent_repr[:-1]}, engine = {self._engine})'
 
     @property
     def engine(self):
@@ -22,3 +26,10 @@ class Car(Vehicle):
             self._engine = engine
         else:
             raise ValueError
+
+
+if __name__ == '__main__':
+    c = Car(100, 20, 30)
+    eng = Engine(2000, 4)
+    c.set_engine(eng)
+    print(c)

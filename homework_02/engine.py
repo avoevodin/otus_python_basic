3572005556bf1b4ced7d72a30e26e30891f79c17
avo888 <avoevodin8888@gmail.com>
2022-04-01 01:04:07 +0300

    added engine data class

--- homework_02/engine.py
+++ homework_02/engine.py
@@ -1,3 +1,13 @@
 """
 create dataclass `Engine`
 """
+from dataclasses import dataclass
+
+
+@dataclass
+class Engine:
+    """
+    Describes an engine entity.
+    """
+    volume: float
+    pistons: int

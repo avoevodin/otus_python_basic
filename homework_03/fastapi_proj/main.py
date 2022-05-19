5f1fd3f94ea6c080059515d6212f3119d167c038
avo888 <avoevodin8888@gmail.com>
2022-05-19 22:20:30 +0300

    added ping endpoint

--- homework_03/fastapi_proj/main.py
+++ homework_03/fastapi_proj/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from starlette.status import HTTP_200_OK
+
 from items_views import router as items_router
-from users.api import router as users_router
 from models_views import router as models_router
+from users.api import router as users_router
 
 app = FastAPI()
 app.include_router(items_router)
@@ -15,3 +17,12 @@ def root():
     :return:
     """
     return {"message": "Hello, World!"}
+
+
+@app.get("/ping/", status_code=HTTP_200_OK)
+def ping():
+    """
+
+    :return:
+    """
+    return {"message": "pong"}

f099727ecec5fbf5dc028a4d4c7285167f5bf84d
avo888 <avoevodin8888@gmail.com>
2022-05-24 15:50:57 +0300

    added config file

---
+++ homework_04/config.py
@@ -0,0 +1,22 @@
+import os
+
+pg_conn_format_string = "postgresql+{driver}://{user}:{pwd}@{host}:{port}/{db_name}"
+
+PG_DRIVER = "asyncpg"
+PG_USER = "pgadmin"
+PG_PWD = "passwd!"
+PG_HOST = "localhost"
+PG_PORT = "5432"
+PG_DB_NAME = "blog"
+
+
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or pg_conn_format_string.format(
+    driver=PG_DRIVER,
+    user=PG_USER,
+    pwd=PG_PWD,
+    host=PG_HOST,
+    port=PG_PORT,
+    db_name=PG_DB_NAME,
+)
+
+PG_DB_ECHO = True

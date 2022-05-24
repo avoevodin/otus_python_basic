48194a0f1ca6e2d43cd0f9da040fe5492e739c10
avo888 <avoevodin8888@gmail.com>
2022-05-24 15:51:24 +0300

    added post model

---
+++ homework_04/models/post.py
@@ -0,0 +1,31 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, String, Text, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .base import Base
+from .mixins import TimestampMixin
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Post(TimestampMixin, Base):
+    title = Column(String(200), nullable=False)
+    body = Column(Text, nullable=False, default="", server_default="")
+
+    user_id = Column(Integer, ForeignKey("users.id"), unique=False, nullable=False)
+    user = relationship("User", back_populates="posts")
+
+    if TYPE_CHECKING:
+        user: User
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"id={self.id}"
+            f", title={self.title}"
+            f", user_id={self.user_id}"
+            f", created_at={self.created_at}"
+            ")"
+        )

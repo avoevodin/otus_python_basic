38733aa2edddea8c17181b3f5facf3220b941518
avo888 <avoevodin8888@gmail.com>
2022-05-24 17:34:02 +0300

    added async db filling with fetched data

--- homework_04/main.py
+++ homework_04/main.py
@@ -15,8 +15,45 @@
 
 import asyncio
 from pprint import pprint
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from jsonplaceholder_requests import fetch_posts_data, fetch_users_data
+from models import User, Post
+from models.base import Session as async_session
+
+
+async def create_users(session: AsyncSession, users_data: List[dict]):
+    users = []
+    for user_data in users_data:
+        user = User(
+            id=user_data["id"],
+            name=user_data["name"],
+            username=user_data["username"],
+            email=user_data["email"],
+        )
+        users.append(user)
+    session.add_all(users)
+
+
+async def create_posts(session: AsyncSession, posts_data: List[dict]):
+    posts = []
+    for post_data in posts_data:
+        post = Post(
+            id=post_data["id"],
+            title=post_data["title"],
+            body=post_data["body"],
+            user_id=post_data["userId"],
+        )
+        posts.append(post)
+    session.add_all(posts)
+
+
+async def create_users_and_posts_in_db(users: List[dict], posts: List[dict]):
+    async with async_session() as session:  # type: AsyncSession
+        await asyncio.gather(create_posts(session, posts), create_users(session, users))
+        await session.commit()
 
 
 async def async_main():
@@ -25,6 +62,8 @@ async def async_main():
     pprint(posts)
     print(">>>>>>> users:")
     pprint(users)
+
+    await create_users_and_posts_in_db(users, posts)
 
 
 def main():
